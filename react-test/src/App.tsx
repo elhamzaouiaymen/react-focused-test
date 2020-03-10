fdc5c4cd1f0614889f0f6207c7b2901977dc3754
aymen hamzaoui <elhamzaouiaymen@gmail.com>
2020-03-10 13:59:05 +0100

    feat: add window resize event change

--- react-test/src/App.tsx
+++ react-test/src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AwesomeComponent from './AwsomeComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AwesomeComponent />
     </div>
   );
 }
